1ca3e13bd5dde90430df18feac7a94e5483b8c01
Jiho Choi <jchoi156@my.centennialcollege.ca>
2024-04-11 19:37:30 -0400

    complete 20

---
+++ src/main/java/com/gp/comp303/PaymentForm.java
@@ -0,0 +1,40 @@
+package com.gp.comp303;
+
+public class PaymentForm {
+    private String cardNumber;
+    private String expirationDate;
+    private String cvv;
+    private String cardHolderName;
+    
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(String expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public String getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(String cvv) {
+        this.cvv = cvv;
+    }
+
+    public String getCardHolderName() {
+        return cardHolderName;
+    }
+
+    public void setCardHolderName(String cardHolderName) {
+        this.cardHolderName = cardHolderName;
+    }
+}
