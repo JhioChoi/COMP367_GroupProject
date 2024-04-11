2bbf0c5d173c15ae4c71b1005a67b837c79eccac
Jiho Choi <jchoi156@my.centennialcollege.ca>
2024-04-11 18:14:25 -0400

    add enrollment model

---
+++ src/main/java/com/gp/comp303/Enrollment.java
@@ -0,0 +1,72 @@
+package com.gp.comp303;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "enrollments")
+public class Enrollment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long applicationNo;
+
+    @ManyToOne
+    @JoinColumn(name = "studentId", referencedColumnName = "studentId")
+    private Student student;
+
+    @ManyToOne
+    @JoinColumn(name = "programCode", referencedColumnName = "programCode")
+    private Program program;
+
+    private LocalDate startDate;
+    private Double amountPaid;
+    private String status;
+    
+    // getters and setters
+	public Long getApplicationNo() {
+		return applicationNo;
+	}
+	public void setApplicationNo(Long applicationNo) {
+		this.applicationNo = applicationNo;
+	}
+	public Student getStudent() {
+		return student;
+	}
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+	public Program getProgram() {
+		return program;
+	}
+	public void setProgram(Program program) {
+		this.program = program;
+	}
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
+	public Double getAmountPaid() {
+		return amountPaid;
+	}
+	public void setAmountPaid(Double amountPaid) {
+		this.amountPaid = amountPaid;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+    
+}
+
