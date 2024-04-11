552f686477755c757d3ed27b168d0648c206290e
Jiho Choi <jchoi156@my.centennialcollege.ca>
2024-04-11 18:00:04 -0400

    first commit

---
+++ src/main/java/com/gp/comp303/GroupProejctComp303Application.java
@@ -0,0 +1,13 @@
+package com.gp.comp303;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupProejctComp303Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupProejctComp303Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gp/comp303/GroupProejctComp303ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gp.comp303;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupProejctComp303ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
