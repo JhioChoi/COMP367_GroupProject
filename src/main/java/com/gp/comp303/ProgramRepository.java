870f7b94a8cb3879286092cf577a3fc6c19ee618
Jiho Choi <jchoi156@my.centennialcollege.ca>
2024-04-11 19:13:37 -0400

    complete 16

---
+++ src/main/java/com/gp/comp303/ProgramRepository.java
@@ -0,0 +1,9 @@
+//Student Name: Jiho Choi
+//Student Number: 301183378
+//Submission date: February 17, 2024
+package com.gp.comp303;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProgramRepository extends CrudRepository<Program, Long> {
+}
